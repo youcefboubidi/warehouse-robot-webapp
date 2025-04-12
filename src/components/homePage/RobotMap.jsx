6ce75edb14d2210e8d8f439e2f4c7d192027754f
akram <son.gogo@hotmail.com>
2025-04-12 05:35:27 +0100

    works wallaho

--- src/components/homePage/RobotMap.jsx
+++ src/components/homePage/RobotMap.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import WebSocketClient from "../navigation/WebClient";
+import MessagesDisplay from "../navigation/MessagesDisplay";
 
 function RobotMap() {
   return (
     <div>
       <h1>Robot Map</h1>
+
+      <WebSocketClient
+        url="ws://localhost:8080"
+        onMessage={(msg) => console.log("Received:", msg)}
+      />
+      <MessagesDisplay />
     </div>
-  )
+  );
 }
 
-export default RobotMap
+export default RobotMap;
